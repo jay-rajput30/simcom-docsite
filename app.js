2cf578982310ca5dd96f80cb59c98d92fab5267f
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2021-04-12 12:20:44 +0530

    feat: updated styles and refactored code

--- app.js
+++ app.js
@@ -3,12 +3,6 @@ const toggleBtn = document.querySelector(".show--hide");
 const sidebarItems = document.querySelectorAll(".doc--name--list--item");
 const description =  document.querySelector(".docs--decription");
 
-// sidebarItems.forEach(item=>{
-//     item.addEventListener("click", function(){
-//         aside.classList.toggle("show");
-//         description.classList.add("description-translate");
-//     })
-// })
 
 toggleBtn.addEventListener("click", function(){
     toggleBtn.classList.toggle("show--hide--rotate");
@@ -20,7 +14,3 @@ toggleBtn.addEventListener("click", function(){
     },5000)
 });
 
-// aside.addEventListener("mouseenter", function(){
-//     aside.classList.add("show");
-
-// })
